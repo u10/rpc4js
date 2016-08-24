3dec15bfa70f4c74ff65246dcf5e03a6d73af1a2
sulj <vaniship@gmail.com>
2016-08-24 15:43:46 +0800

    发布版本0.1.0

--- dist/rpc4js.js
+++ dist/rpc4js.js
@@ -1,5 +1,5 @@
 /*!
- * rpc4js v0.0.1
+ * rpc4js v0.1.0
  * (c) 2016 Vaniship
  * Released under the MIT License.
  */
@@ -16,54 +16,55 @@
     var slice = [].slice;
     var localConstructors = {};
 
-    function mapping(obj, funcIndex, funcMap, opts, path, context) {
+    function release(id, funcMap) {
+        if (funcMap[id]) {
+            for (var index in funcMap[id]) {
+                delete funcMap[funcMap[id][index]];
+            }
+            funcMap[id] = false;
+            setTimeout(function () {
+                delete funcMap[id];
+            }, 100);
+        }
+    }
+
+    function clear(funcIndex, funcMap) {
+        for (var index in funcIndex) {
+            var key = funcIndex[index];
+            delete funcMap[key[key.length - 1]];
+        }
+    }
+
+    function mapping(obj, funcIndex, funcMap, options, path, context) {
         path = path || [];
         if (_.isFunction(obj)) {
             var _ret = (function () {
                 var id = uuid.v4();
-                if (opts && opts.clear) {
-                    opts.clear.push(id);
+                if (!options.autoRelease && options.id) {
+                    if (options.disposed || funcMap[options.id] === false) {
+                        return {
+                            v: 0
+                        };
+                    }
+                    (funcMap[options.id] = funcMap[options.id] || []).push(id);
                 }
-                funcMap[id] = (function (context) {
-                    return function () {
-                        var rpc = _.extend({}, opts, { clear: false });
-                        var args = 1 <= arguments.length ? slice.call(arguments, 0) : [];
-                        var result = obj.apply(context, [rpc].concat(args));
-                        if (rpc.clear) {
-                            if (opts.clear) {
-                                var _iteratorNormalCompletion = true;
-                                var _didIteratorError = false;
-                                var _iteratorError = undefined;
-
-                                try {
-                                    for (var _iterator = opts.clear[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-                                        var v = _step.value;
-
-                                        delete funcMap[v];
-                                    }
-                                } catch (err) {
-                                    _didIteratorError = true;
-                                    _iteratorError = err;
-                                } finally {
-                                    try {
-                                        if (!_iteratorNormalCompletion && _iterator['return']) {
-                                            _iterator['return']();
-                                        }
-                                    } finally {
-                                        if (_didIteratorError) {
-                                            throw _iteratorError;
-                                        }
-                                    }
-                                }
-                            }
-                        } else {
-                            if (rpc.release) {
-                                delete funcMap[id];
-                            }
+                funcMap[id] = function () {
+                    var deferred = Q.defer();
+                    var args = 1 <= arguments.length ? slice.call(arguments, 0) : [];
+                    try {
+                        deferred.resolve(obj.apply(context, args));
+                        if (options.autoRelease) {
+                            delete funcMap[id];
                         }
-                        return result;
-                    };
-                })(context);
+                    } catch (error) {
+                        deferred.reject({ message: error.message });
+                        if (options.autoRelease) {
+                            funcIndex = clear(funcIndex, funcMap);
+                            delete funcMap[id];
+                        }
+                    }
+                    return deferred.promise;
+                };
                 funcIndex.push(path.concat([id]));
                 return {
                     v: 0
@@ -75,7 +76,7 @@
             var val = [];
             for (var i = 0, len = obj.length; i < len; i++) {
                 path.push(i);
-                val.push(mapping(obj[i], funcIndex, funcMap, opts, path));
+                val.push(mapping(obj[i], funcIndex, funcMap, options, path));
                 path.pop();
             }
             return val;
@@ -83,7 +84,7 @@
             var val = {};
             for (var k in obj) {
                 path.push(k);
-                val[k] = mapping(obj[k], funcIndex, funcMap, opts, path, obj);
+                val[k] = mapping(obj[k], funcIndex, funcMap, options, path, obj);
                 path.pop();
             }
             return val;
@@ -92,7 +93,7 @@
         }
     }
 
-    function proxy(ws, obj, funcIndex, funcMap) {
+    function proxy(ws, obj, funcIndex, funcMap, options) {
         var method, o, v;
         for (var i = 0, m = funcIndex.length; i < m; i++) {
             v = funcIndex[i];
@@ -103,61 +104,94 @@
             method = v[v.length - 1];
             o[v[v.length - 2]] = (function (method) {
                 return function () {
-                    var args = 1 <= arguments.length ? slice.call(arguments, 0) : [];
                     var deferred = Q.defer();
-                    var funcIndex = [];
+                    var args = 1 <= arguments.length ? slice.call(arguments, 0) : [];
                     var id = uuid.v4();
-                    ws.emit('rpc-call', {
-                        id: id,
-                        method: method,
-                        args: mapping(args, funcIndex, funcMap, { release: true, clear: [] }),
-                        funcIndex: funcIndex
-                    });
-                    var timer = setTimeout(function () {
-                        ws.removeListener('rpc-return', callback);
-                        deferred.reject();
-                    }, 5000);
-                    var callback = function callback(res) {
-                        if (res.id === id) {
-                            clearTimeout(timer);
+                    var _config = _.extend({
+                        timeout: 5000,
+                        autoRelease: true,
+                        id: id
+                    }, options);
+                    var funcIndex = [];
+                    setTimeout(function () {
+                        var timer = setTimeout(function () {
                             ws.removeListener('rpc-return', callback);
-                            if (res.error) {
-                                deferred.reject(res.error);
-                            } else {
-                                deferred.resolve(res.result);
+                            deferred.reject({ message: 'timeout' });
+                        }, _config.timeout);
+                        var callback = function callback(res) {
+                            if (res.id === id) {
+                                clearTimeout(timer);
+                                ws.removeListener('rpc-return', callback);
+                                if (res.error) {
+                                    deferred.reject(res.error);
+                                    if (_config.autoRelease) {
+                                        funcIndex = clear(funcIndex, funcMap);
+                                        delete funcMap[method];
+                                    }
+                                } else {
+                                    deferred.resolve(res.result);
+                                    if (_config.autoRelease) {
+                                        delete funcMap[method];
+                                    }
+                                }
+                            }
+                        };
+                        ws.emit('rpc-call', {
+                            id: id,
+                            method: method,
+                            args: mapping(args, funcIndex, funcMap, _config),
+                            funcIndex: funcIndex,
+                            options: _config
+                        });
+                        ws.on('rpc-return', callback);
+                    }, 0);
+                    var promise = _.extend(deferred.promise, {
+                        config: function config(conf) {
+                            _.extend(_config, conf);
+                            return promise;
+                        },
+                        end: function end() {
+                            if (!_config.autoRelease) {
+                                _config.disposed = true;
+                                release(id, funcMap);
+                                ws.emit('rpc-release', { id: id });
                             }
                         }
-                    };
-                    ws.on('rpc-return', callback);
-                    return deferred.promise;
+                    });
+                    return promise;
                 };
             })(method);
         }
         return obj;
     }
 
-    function mkRpcCallCallback(ws, funcMap) {
+    function rpcCallCallbackFactory(ws, funcMap) {
         return function (req) {
             var func = funcMap[req.method];
             if (func) {
-                try {
-                    var result = func.apply(null, proxy(ws, req.args, req.funcIndex, funcMap));
+                func.apply(null, proxy(ws, req.args, req.funcIndex, funcMap, req.options)).then(function (result) {
                     ws.emit('rpc-return', {
                         id: req.id,
                         result: result
                     });
-                } catch (e) {
+                }).fail(function (error) {
                     ws.emit('rpc-return', {
                         id: req.id,
-                        error: e
+                        error: error
                     });
-                }
+                });
             } else {
                 ws.emit('rpc-return', {
                     id: req.id,
-                    error: 'function not found'
+                    error: { message: 'function not found' }
                 });
             }
+        };
+    }
+
+    function rpcReleaseCallbackFactory(funcMap) {
+        return function (req) {
+            release(req.id, funcMap);
         };
     }
 
@@ -175,18 +209,19 @@
                     var constructor = localConstructors[req.name];
                     if (constructor) {
                         var local = constructor(proxy(socket, req.client, req.funcIndex, funcMap));
-                        socket.on('rpc-call', mkRpcCallCallback(socket, funcMap));
+                        socket.on('rpc-call', rpcCallCallbackFactory(socket, funcMap));
+                        socket.on('rpc-release', rpcReleaseCallbackFactory(funcMap));
                         socket.emit('rpc-connect', {
                             id: req.id,
                             name: req.name,
-                            remote: mapping(local, funcIndex, funcMap),
+                            remote: mapping(local, funcIndex, funcMap, req.options),
                             funcIndex: funcIndex
                         });
                     } else {
                         socket.emit('rpc-connect', {
                             id: req.id,
                             name: req.name,
-                            error: 'object not found'
+                            error: { message: 'object not found' }
                         });
                     }
                 }).on('disconnect', function () {
@@ -199,27 +234,34 @@
             this.ws = ws;
             return this;
         },
-        connect: function connect(name, local) {
+        connect: function connect(name, local, options) {
+            options = _.extend({
+                timeout: 5000,
+                autoRelease: false
+            }, options);
             var deferred = Q.defer();
             var ws = this.ws;
             var funcIndex = [];
             var funcMap = {};
+
             function release() {
                 clearTimeout(timer);
                 ws.removeListener('rpc-connect', rpcConnectCallback);
                 funcMap = undefined;
                 funcIndex = undefined;
             }
+
             var timer = setTimeout(function () {
                 release();
-                deferred.reject('timeout');
-            }, 5000);
+                deferred.reject({ message: 'timeout' });
+            }, options.timeout);
             var id = uuid.v4();
             ws.emit('rpc-connect', {
                 id: id,
                 name: name,
-                client: mapping(local, funcIndex, funcMap),
-                funcIndex: funcIndex
+                client: mapping(local, funcIndex, funcMap, options),
+                funcIndex: funcIndex,
+                options: options
             });
             var rpcConnectCallback = function rpcConnectCallback(res) {
                 if (res.id === id) {
@@ -229,18 +271,24 @@
                     } else {
                         clearTimeout(timer);
                         ws.removeListener('rpc-connect', rpcConnectCallback);
-                        ws.on('rpc-call', mkRpcCallCallback(ws, funcMap));
+                        ws.on('rpc-call', rpcCallCallbackFactory(ws, funcMap));
+                        ws.on('rpc-release', rpcReleaseCallbackFactory(funcMap));
                         deferred.resolve(proxy(ws, res.remote, res.funcIndex, funcMap));
                     }
                 }
             };
             ws.on('rpc-connect', rpcConnectCallback);
-            return deferred.promise;
+            return _.extend(deferred.promise, {
+                check: function check() {
+                    console.log(funcMap);
+                }
+            });
         },
         destroy: function destroy() {
             var ws;
             ws = this.ws;
             ws.removeListener('rpc-call');
+            ws.removeListener('rpc-release');
             ws.removeListener('rpc-return');
             delete this.ws;
         }
